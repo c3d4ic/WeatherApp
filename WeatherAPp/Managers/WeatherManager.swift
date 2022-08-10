d56ef538721e0b3985b175cd2ba6366cf3fe52ea
Cédric <cchabaud@interludesante.com>
2022-08-10 15:19:37 +0200

    first commit

---
+++ WeatherAPp/Managers/WeatherManager.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherManager.swift
+//  WeatherAPp
+//
+//  Created by Cédric Chabaud on 05/08/2022.
+//
+
+import Foundation
+import CoreLocation
+
+class WeatherManager {
+    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody{
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\("af4bff991bf6ea65dd38625b0c14db81")&units=metric&lang=fr") else { fatalError("Missing URL")}
+        
+        let urlRequest = URLRequest(url: url)
+        
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error fetching weather data")}
+        
+        let decodedData = try JSONDecoder().decode(ResponseBody.self, from: data)
+        
+        return decodedData
+    }
+}
+
+
---
+++ WeatherAPp/WeatherAPpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPpApp.swift
+//  WeatherAPp
+//
+//  Created by Cédric Chabaud on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
